745e47abd08f6076f8fdbd4b4c208ca963056350
lovtens <lovefh29@gmail.com>
2021-10-14 23:12:34 +0900

    daily commit

---
+++ 47.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/permutations-ii/
+
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+        n = len(nums)
+        result = []
+
+        def generate(comb: List[int], hashtable: dict):
+            if len(comb) == n:
+                result.append(comb[:])
+
+            for k, v in hashtable.items():
+                if v < 1:
+                    continue
+                comb.append(k)
+                hashtable[k] -= 1
+                generate(comb, hashtable)
+                comb.pop()
+                hashtable[k] += 1
+
+        counter = Counter(nums)
+
+        generate([], counter)
+        return result
