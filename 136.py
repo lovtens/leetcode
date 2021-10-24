3b4aeaedd49e2e6488b6a16e4c5be769a7942f94
lovtens <lovefh29@gmail.com>
2021-10-24 22:32:49 +0900

    daily commit

--- 136.py
+++ 136.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/problems/single-number/
-
+from functools import reduce
 from typing import List
 
 
@@ -11,3 +11,8 @@ class Solution:
         for k, v in hash_table.items():
             if v == 1:
                 return k
+
+    def singleNumber2(self, nums: List[int]) -> int:
+        # 0^n = n
+        # n^n = 0
+        return reduce((lambda x, y: x ^ y), nums)
