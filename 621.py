99da4ecd704c053ca93827b42139e91ed9e9a25d
lovtens <lovefh29@gmail.com>
2021-10-28 20:28:08 +0900

    daily commit

--- 621.py
+++ 621.py
@@ -1,61 +1,41 @@
-from collections import Counter
+# https://leetcode.com/problems/task-scheduler/submissions/
+# greedy heap priority queue
+
+from collections import Counter, deque
 from queue import PriorityQueue
 from typing import List
 
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        counter = Counter(tasks)
-        recent = {key: -n - 1 for key in counter.keys()}
-        pq = PriorityQueue()
-        for k, v in counter.items():
-            pq.put((-v, k))
+        if n == 0:
+            return len(tasks)
 
+        counter = Counter(tasks)
+        pq = PriorityQueue()
+        cooling = deque()
+        # result = []
         i = 0
-        tmp = PriorityQueue()
-        while True:
-            # print('tmp', tmp.queue)
-            # print('pq', pq.queue)
-            # print('result', result)
-            # print('----')
+        for k, v in counter.items():
+            pq.put((-v, -3, k))
+        while not pq.empty() or len(cooling) > 0:
             if pq.empty():
-                if tmp.empty():
-                    break
-                while True:
-                    if tmp.empty():
-                        break
-                    a = tmp.get()
-                    r, pair = a
-                    if r + n < i + 1:
-                        pq.put(pair)
-                    else:
-                        tmp.put((r, pair))
-                        break
-                i += 1
-                # result.append('idle')
-            else:
-                pair = pq.get()
-                v, k = pair
-                if i <= recent[k] + n:
-                    tmp.put((recent[k], pair))
-                else:
-                    recent[k] = i
-                    i += 1
-                    # result.append(k)
-                    if -v - 1 > 0:
-                        pq.put((v + 1, k))
-                    while True:
-                        if tmp.empty():
-                            break
-                        r, pair = tmp.get()
-                        if r + n < i:
-                            pq.put(pair)
-                        else:
-                            tmp.put((r, pair))
-                            break
+                i = cooling[0][1] + n + 1
+
+            while len(cooling) > 0 and cooling[0][1] + n < i:
+                pq.put(cooling.popleft())
+
+            v, recent, k = pq.get()
+            # result.append(k)
+            if v + 1 < 0:
+                # n > 0이니까 무조건 쿨링으로
+                cooling.append((v + 1, i, k))
+            i += 1
+
         # print(result)
         return i
 
 
-result = Solution().leastInterval(["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"], 2)
-print(result)
+tasks = ["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"]
+n = 2
+result = Solution().leastInterval(tasks, n)
